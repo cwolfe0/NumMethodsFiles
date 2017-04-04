0b882d7d2f9c7d61603a586d47c35f4a73d5d06a
cory <wolfe.cory1@gmail.com>
2017-04-04 14:26:56 -0500

    New Bike Data

--- bikeData.m
+++ bikeData.m
@@ -3,13 +3,11 @@
 M = csvread('bike.csv');
 t0 = M(1,1);
 M(:,1) = M(:,1)-t0;
-% for(i = 1:length(M))
-%     if(M(i,2) ~=0)
-%         M(i,2) = 0;
-%     else
-%         M(i,2) = 1;
-%     end
-% end
 plot(M(:,1),M(:,3))
 hold
 plot(M(:,1),M(:,4))
+hold off
+legend('Total Distance (ft)','RPM');
+title('Plot of RPM and Total Distance');
+xlabel('Time (ms)');
+ylabel('Total Distance(ft) and RPM');
